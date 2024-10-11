993367030ab3256b2b7ccf3259b730b5f887d2bf
Ivan Ng <147109497+ivng8@users.noreply.github.com>
2024-10-11 14:25:11 -0400

    Update http.util.ts

--- client/src/utils/http.util.ts
+++ client/src/utils/http.util.ts
@@ -2,6 +2,21 @@ import axios, { Axios, AxiosResponse } from 'axios';
 
 export class HttpUtil {
 
-  // private _axios: Axios;
+  private _axios: Axios;
 
+  constructor(baseURL: string) {
+    this._axios = axios.create({
+      baseURL: baseURL
+    });
+  }
+
+  public async getRequest<T>(): Promise<T> {
+    const response: AxiosResponse = await this._axios.get('');
+    return response.data;
+  }
+
+  public async postRequest<S, T>(requestBody: T): Promise<S> {
+    const response: AxiosResponse = await this._axios.post('', requestBody);
+    return response.data;
+  }
 }

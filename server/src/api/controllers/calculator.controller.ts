af8e2ef3b89ca2e09223b2a4b94cab08fc5c8052
Ivan Ng <147109497+ivng8@users.noreply.github.com>
2024-10-11 14:26:12 -0400

    Update calculator.controller.ts

--- server/src/api/controllers/calculator.controller.ts
+++ server/src/api/controllers/calculator.controller.ts
@@ -6,12 +6,29 @@ import { ActionKeys, NumericKeys, OperatorKeys } from '../../enums';
 
 export class CalculatorController {
 
+  private static calculatorModel: ICalculatorModel = new CalculatorModel();
+
   public static getHandler(req: Request, res: Response<string>): void {
-    res.status(500).json('Internal Server Error');
+    res.status(200).json(CalculatorController.calculatorModel.display());
   }
 
   public static postHandler(req: Request<RequestBody>, res: Response<string>): void {
-    res.status(500).json('Internal Server Error');
+    const body: RequestBody = req.body;
+    switch (body.operationType) {
+      case 'NumericKeys':
+        CalculatorController.calculatorModel.pressNumericKey(<NumericKeys>body.value);
+        break;
+      case 'OperatorKeys':
+        CalculatorController.calculatorModel.pressOperatorKey(<OperatorKeys>body.value);
+        break;
+      case 'ActionKeys':
+        CalculatorController.calculatorModel.pressActionKey(<ActionKeys>body.value);
+        break;
+      default:
+        res.status(500).json('ERR');
+        return;
+    }
+    res.status(200).json(CalculatorController.calculatorModel.display());
   }
 
   public static allHandler(req: Request, res: Response): void {
